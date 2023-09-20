427b995b9059afb1ce8fa828812393f30f7b3cf2
Kleiton Silva Souza de Azevedo <kleiton.ads@hotmail.com>
2023-09-20 18:01:12 -0300

    add files
    add files and fix layout

--- assets/javascript/functions.js
+++ assets/javascript/functions.js
@@ -1,4 +1,29 @@
 function switchbutton() {
   const html = document.documentElement;
-  html.classList.toggle("dark")
+  html.classList.toggle("dark");
+
+  const imgTitle = document.querySelector(".title-notes img");
+ 
+  const switchIcon = document.querySelector(".title-notes img");
+
+  // console.log(switchIcon)
+
+  if (html.classList.contains("dark")) {
+     imgTitle.setAttribute("src", "./assets/img/draft-line.svg")
+
+
+  } else {
+    imgTitle.setAttribute("src", "./assets/img/draft-line-dark.svg")
+
+  }
+
+
+
+
+
+
 }
+
+
+
+
