8fd2e63fdd7157b04c7f8c974d78629697157a96
Kleiton Silva Souza de Azevedo <kleiton.ads@hotmail.com>
2023-09-20 22:29:44 -0300

    add files
    layout done

--- assets/javascript/functions.js
+++ assets/javascript/functions.js
@@ -3,17 +3,25 @@ function switchbutton() {
   html.classList.toggle("dark");
 
   const imgTitle = document.querySelector(".title-notes img");
- 
-  const switchIcon = document.querySelector(".title-notes img");
 
-  // console.log(switchIcon)
+  const imgDelete = document.querySelector(".input-box img");
 
-  if (html.classList.contains("dark")) {
-     imgTitle.setAttribute("src", "./assets/img/draft-line.svg")
+  const imgButtonNotes = document.querySelector(".button-notes img")
+
+   if (html.classList.contains("dark")) {
+     imgTitle.setAttribute("src", "./assets/img/draft-line-w.svg")
+
+     imgDelete.setAttribute("src", "./assets/img/delete-bin-line-w.svg")
+
+     imgButtonNotes.setAttribute("src", "./assets/img/edit-line-w.svg")
 
 
   } else {
-    imgTitle.setAttribute("src", "./assets/img/draft-line-dark.svg")
+    imgTitle.setAttribute("src", "./assets/img/draft-line.svg")
+    
+    imgDelete.setAttribute("src", "./assets/img/delete-bin-line.svg")
+
+    imgButtonNotes.setAttribute("src", "./assets/img/edit-line.svg")
 
   }
 
