9fe05bbd4e5f6adc0aa5c0d307ecaa16cd30dffe
Kleiton Silva Souza de Azevedo <kleiton.ads@hotmail.com>
2023-09-27 22:32:01 -0300

    add functions
    add function notes add localstorage

--- assets/javascript/functions.js
+++ assets/javascript/functions.js
@@ -1,35 +1,121 @@
+    const notesContainer = document.querySelector(".notes-container");
+    const createBtn = document.querySelector(".button-notes")
+    let notes = document.querySelectorAll(".input-box")
+    let html = document.documentElement;
+
+    let imgTitle = document.querySelector(".title-notes img");
+
+    let imgButtonNotes = document.querySelector(".button-notes img")
+    
+
+    function showNotes() {
+      const theme = localStorage.getItem('website_theme');
+      if(theme != null){
+        html.classList.remove('default', 'dark'); 
+        html.classList.add(theme);
+     }
+
+      notesContainer.innerHTML = localStorage.getItem("notes")
+
+
+     if (html.classList.contains("dark")) {
+        imgTitle.setAttribute("src", "./assets/img/draft-line-w.svg")
+
+        imgButtonNotes.setAttribute("src", "./assets/img/edit-line-w.svg")
+    } else {
+        imgTitle.setAttribute("src", "./assets/img/draft-line.svg")
+
+        imgButtonNotes.setAttribute("src", "./assets/img/edit-line.svg")
+    }
+
+
+
+    }
+
+    showNotes()
+
+
+    function updateStorage(){
+
+      localStorage.setItem("notes", notesContainer.innerHTML)
+  
+    }
+
+    createBtn.addEventListener("click", ()=>{
+  
+      let inputBox = document.createElement("p")
+      let img = document.createElement("img");
+      inputBox.className = "input-box";
+      inputBox.setAttribute("contenteditable", "true")
+      
+
+    if (html.classList.contains("dark")) {
+     
+      img.src = "./assets/img/delete-bin-line-w.svg"
+
+    } else {
+      img.src = "./assets/img/delete-bin-line.svg"
+    }
+
+
+      notesContainer.appendChild(inputBox).appendChild(img)
+    })
+
+    notesContainer.addEventListener("click", e =>{
+      
+      notes = document.querySelectorAll(".input-box");
+      if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+        updateStorage();
+        console.log("save remove")
+      }
+      else if( e.target.tagName === "P"){
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+          nt.onkeyup = function(){
+            updateStorage();
+            console.log("save notes")
+          }
+        })
+      }
+    })
+
+    document.addEventListener("keydown", event =>{
+      if(event.key === "Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+      }
+    })
+
+
+
+
+
+
 function switchbutton() {
-  const html = document.documentElement;
+  html = document.documentElement;
   html.classList.toggle("dark");
 
-  const imgTitle = document.querySelector(".title-notes img");
+  imgTitle = document.querySelector(".title-notes img");
 
-  const imgDelete = document.querySelector(".input-box img");
-
-  const imgButtonNotes = document.querySelector(".button-notes img")
+  imgButtonNotes = document.querySelector(".button-notes img")
 
    if (html.classList.contains("dark")) {
      imgTitle.setAttribute("src", "./assets/img/draft-line-w.svg")
 
-     imgDelete.setAttribute("src", "./assets/img/delete-bin-line-w.svg")
-
      imgButtonNotes.setAttribute("src", "./assets/img/edit-line-w.svg")
+
+     localStorage.setItem('website_theme','dark');
 
 
   } else {
     imgTitle.setAttribute("src", "./assets/img/draft-line.svg")
-    
-    imgDelete.setAttribute("src", "./assets/img/delete-bin-line.svg")
 
     imgButtonNotes.setAttribute("src", "./assets/img/edit-line.svg")
 
+    localStorage.setItem('website_theme','default');
+
   }
-
-
-
-
-
-
 }
 
 
